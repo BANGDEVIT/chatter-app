fc4a17e1917eaddea2dd642a334736bd7a232346
Bang <buicongbang1205@gmail.com>
2025-10-27 01:04:51 +0700

    deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import dotenv from "dotenv";
+import path from "path";
 
 import { ENV } from "./lib/env.js";
 import { connectDB } from "./lib/db.js";
@@ -10,6 +11,8 @@ import { clientRoutes } from "./routes/index.route.js";
 dotenv.config();
 
 const app = express();
+
+const __dirname = path.resolve();
 const PORT = ENV.PORT || 3000;
 
 app.use(express.json({ limit: "5mb" })); // dùng để gửi req.body và giới hạn gửi lên là 5mb
@@ -17,7 +20,16 @@ app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
 app.use(cookieParser());
 
 //ClientRoutes
-clientRoutes(app); 
+clientRoutes(app);
+
+// make ready for deployment
+if (ENV.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (_, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 connectDB().then(() => {
   app.listen(PORT, () => {
